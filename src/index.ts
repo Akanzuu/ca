edfa11f815ce63451bed08880ae5a311a32d8131
zachariapopcorn <zachariahmad@gmail.com>
2023-02-13 18:36:41 -0800

    Get beginning stuff out of the way

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,13 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import * as fun from 'funcaptcha';
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
+	API_KEY: string
 }
 
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		return new Response("Hello World!");
+	async fetch(request: Request, env: Env): Promise<Response> {
+		let key = request.headers.get("api-key");
+		if(key !== env.API_KEY) return new Response("Invalid API key");
+		return new Response("hello")
 	},
 };
